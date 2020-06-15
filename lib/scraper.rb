e2ead5774b081bf04d201f79e2a44a49ed97e924
Luke Morrison <lukejamesmorrison@gmail.com>
2020-06-15 20:07:13 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,10 +4,11 @@ require 'pry'
 class Scraper
 
   def self.scrape_page(url)
+    Nokogiri::HTML(open(url))
   end
 
   def self.scrape_index_page(index_url)
-    doc = Nokogiri::HTML(open(index_url))
+    doc = 
   end
 
   def self.scrape_profile_page(profile_url)
