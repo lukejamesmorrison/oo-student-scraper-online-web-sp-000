5fbffcaa4d0a1e1d9dd34cf8cd12bd29bb609acd
Luke Morrison <lukejamesmorrison@gmail.com>
2020-06-15 20:07:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,11 +8,11 @@ class Scraper
   end
 
   def self.scrape_index_page(index_url)
-    doc =
+    doc = self.scrape_page(index_url)
   end
 
   def self.scrape_profile_page(profile_url)
-
+    doc = self.scrape_page(profile_url)
   end
 
 end
